1a4357bcf39cefcef22f25c37e243d95e33464f9
sphericalice <sphericalice@hotmail.co.uk>
2016-03-20 16:15:34 +0000

    Ini

---
+++ MassCompatibilityEditor/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MassCompatibilityEditor")]
+[assembly: AssemblyDescription("Quickly edit the TM and HM compatibility of large amounts of Pokémon.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Spherical Ice")]
+[assembly: AssemblyProduct("MassCompatibilityEditor")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("85e4330b-67e9-4339-814f-29a58ba16b34")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
